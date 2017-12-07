32d455876e5972e17632a325450ea0bcf5033ab2
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-12-07 13:11:58 -0500

    finished

--- Sortamania/src/TeamSortCompetition.java
+++ Sortamania/src/TeamSortCompetition.java
@@ -4,7 +4,8 @@ public class TeamSortCompetition {
 	public static void main (String[] args)
 	{
 		int [] newarray = {14,6,9,2,8,9,11,5,24,10};
-		int [] inputTenThousand = new int[10000];              
+		int [] inputTenThousand = new int[10000];      
+		String[] x1= {"food","zebra","batman","superman","flash"};
 	    for (int a = 0; a < inputTenThousand.length; a++) {
 	       inputTenThousand [a] = (int) (Math.random () * 10000);
 	    }
@@ -23,11 +24,11 @@ public class TeamSortCompetition {
 		long totalTime1 = endTime1 - startTime1;
 		System.out.println("Time Taken in nanoseconds: " + totalTime1);
 		
-		int[][] arr1 = new int[10000][10000];
+		int[][] arr1 = new int[1000][1000];
 
-	    for (int x = 0; x < 10000; x++) {
-	        for (int y = 0; y < 10000; y++) {
-	            arr1[x][y] = (int) (Math.random () * 1000);;
+	    for (int x = 0; x < 1000; x++) {
+	        for (int y = 0; y < 1000; y++) {
+	            arr1[x][y] = (int) (Math.random () * 10000);;
 	        }   
 	    } 
 	    
@@ -36,6 +37,12 @@ public class TeamSortCompetition {
 		long endTime3 = System.nanoTime();
 		long totalTime3 = endTime3 - startTime3;
 		System.out.println("Time Taken in nanoseconds: " + totalTime3);
+		
+		long startTime4 = System.nanoTime();
+		System.out.print(challengeFive(x1,"zebra"));
+		long endTime4 = System.nanoTime();
+		long totalTime4 = endTime4 - startTime4;
+		System.out.println("Time Taken in nanoseconds: " + totalTime4);
 	
 	}
 	public static int challengeOne(int[] arr)
@@ -75,18 +82,90 @@ public class TeamSortCompetition {
 		median = getmedian(arr);
 		return median;
 	}
-	public static double challengeFour(int[][] arr)
+	public static int challengeFour(int[][] arr)
 	{
-		double[] medval= new double[arr.length];
-		int i=0;
+		int[] medval= new int[arr.length];
 		for(int j=0; j<arr.length;j++)
 		{
-			medval[i]=challengeOne(arr[j]);
-			i++;
+			medval[j]=challengeThree(arr[j]);
 		}
-		
-		return getdoublemed(medval);
+		for(int i=0;i<medval.length;i++)
+		{
+			for (int j = i; j > 0; j--)	
+			{
+				if(medval[j] < medval[j-1])
+				{
+					swap(medval,j,j-1);
+				}
+			}
+		}
+		return getmedian(medval);
 	}
+	
+	
+	public static int challengeFive(Comparable[] arr, Comparable query)
+	{
+		mergeSort(arr);
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==query)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static Comparable[] mergecomparable(Comparable[] list1, Comparable[] list2)
+	{
+		int newlist = 0;
+		int newlist2 = 0;
+		int newlist3 = 0;
+		Comparable[] newarray = new Comparable[list1.length + list2.length];
+		while(newlist < list1.length || newlist2 < list2.length)
+		{
+			if (newlist == list1.length)
+			{
+				newarray[newlist3] = list2[newlist2];
+				newlist2++;
+			}
+			else if (newlist2 == list2.length)
+			{
+				newarray[newlist3] = list1[newlist];
+				newlist++;
+			}
+			else if (list1[newlist].compareTo(list2[newlist2]) < 0)
+			{
+				newarray[newlist3] = list1[newlist];
+				newlist++;
+			}
+			else 
+			{
+				newarray[newlist3] = list2[newlist2];
+				newlist2++;
+			}
+			newlist3++;
+		}
+		return newarray;
+	}
+	public static Comparable[] mergeSort(Comparable[] list)
+	{
+		if (list.length <= 1) 
+		{
+			return list;
+		}
+		else
+		{
+			int x=list.length/2;
+			Comparable[] temp1 = Arrays.copyOfRange(list,0,x);
+			Comparable[] temp2 = Arrays.copyOfRange(list,x,list.length);
+			return mergecomparable(mergeSort(temp1),mergeSort(temp2));
+		}
+	}
+	
+	
+	
+	
 	public static void swap(int[]arr,int i,int j)
 	{
 		int x = arr[i];
@@ -199,18 +278,5 @@ public class TeamSortCompetition {
 	        randomStrings[i] = new String(word);
 	    }
 	    return randomStrings;
-	}
-	public static double getdoublemed(double[] arr)
-	{
-		double median = 0;
-		if (arr.length % 2 == 1)
-		{
-			median = (double)arr[arr.length/2];
-		}
-		else
-		{
-			median = ((double)arr[arr.length/2] + (double)arr[(arr.length/2)-1])/2;
-		}
-		return median;
 	}
 }
